010aad52a966eecf374c82407bf47e59128fa374
Crispin Mushengezi <crispincirhuza749@gmail.com>
2026-09-19 16:12:42 +0200

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
-      <h1 className="text-3xl font-bold text-blue-600 underline">
-        Tailwind fonctionne avec React !
-      </h1>
-      <button className="btn btn-primary">Mon bouton DaisyUI</button>
-    </div>
+   <div>
+    <h1>C'est mon application React !</h1>
+    <Link to="/inscription">Inscription</Link>
+   </div>
   )
 }
 
