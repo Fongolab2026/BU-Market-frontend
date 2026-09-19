d26a2688ae8c4f9979fba456b2016e1ff8b6bc68
jean-victoire3 <victoirekalombo6@gmail.com>
2026-09-19 17:41:01 +0200

    hooks api

---
+++ src/Users/Hooks/hooks.jsx
@@ -0,0 +1,29 @@
+import { useState,useEffect } from "react"
+import {api} from "../../data/api"
+
+export default function useProducts(url ){
+    const [loading,setLoading]=useState(false)
+    const [data,setData]= useState([])
+    const [err,setErr]= useState(null)
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            setLoading(true)
+            try{
+                const rep = await api.get(url)
+                if(rep.status!==200){
+                    throw new Error(`echec de chargement statut ${rep.status}`)
+                }
+                const prods = await rep.data.products
+                setData(prods)
+            }catch(e){
+                setErr(e instanceof Error? e: new Error("une erreur est arrivee"))
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        fetchData()
+    },[url])
+    return {data,err,loading}
+    
+}
