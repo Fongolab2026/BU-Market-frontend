750068e488695f3477a39c925b02511e35d5ee03
Crispin Mushengezi <crispincirhuza749@gmail.com>
2026-09-19 17:07:20 +0200

    correction

---
+++ src/Users/auth/Connexion.jsx
@@ -0,0 +1,9 @@
+export default function Connexion() {
+  return (
+    <div className="flex justify-center h-screen p-1 items-center bg-[whitesmoke]">
+      <div className="w-105 rounded-md gap-3 flex flex-col p-10 bg-white shadow-md text-center">
+        <h1 className="text-[1.8rem] font-bold text-gray-700">Se connecter</h1>
+      </div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import ReactDom from 'react-dom/client';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './Users/routes/AppRouter.jsx';
+import App from './App.jsx';
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <>
   <Toaster/>
-  <RouterProvider router={router}/>
+  <App/>
   </>
-
 )
